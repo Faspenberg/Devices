8bcf9a3e68ee01dc8fe475a89e332f72399945af
Fredrik Aspenberg <aspenberg16@hotmail.com>
2023-10-12 21:43:01 +0200

    Add project files.

---
+++ Device.IntelliFan/Models/Devices/DirectMethodDataResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Device.IntelliFan.Models.Devices
+{
+    public class DirectMethodDataResponse
+    {
+        public string? Message { get; set; }
+
+        public string? Payload { get; set; }
+
+    }
+}
